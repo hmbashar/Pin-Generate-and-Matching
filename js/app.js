eda9b69d776abddae73db1afb4c1ea4c2a658da4
hmbashar <hmbashar@gmail.com>
2023-02-24 15:30:58 +0600

    remove last number

--- js/app.js
+++ js/app.js
@@ -18,9 +18,37 @@ function generatePin() {
     return random;
 }
 
+
 document.getElementById('btn-generate-pin').addEventListener('click', function() {
     const pin = getPin();
     // display pin
     const displayPinField = document.getElementById('display-pin');
     displayPinField.value = pin;
+
+});
+
+
+document.getElementById('calculator').addEventListener('click', function(event) {
+    const clickedNumber = event.target.innerText;    // catch clicked number value
+    const typedNumberField = document.getElementById('typed-number'); // get input field value
+    const previousTypedNumber = typedNumberField.value; // get previous typed number
+
+    //check if isn't a number
+    if(isNaN(clickedNumber)) {
+        // clear the input filed if clicked C
+        if(clickedNumber === 'C') {
+            typedNumberField.value = ''; 
+        }
+        else if(clickedNumber === '<') { // remove last number
+            const digits = previousTypedNumber.split(''); //convert string to array
+            digits.pop(); // remove last array
+            const remainingDigits = digits.join(''); // join array to string
+            typedNumberField.value = remainingDigits; // set updated string
+        }
+    }else {       
+        const newTypedNumber = previousTypedNumber + clickedNumber; // concat previous and new number (It's like a string data so don't need to convert)
+        typedNumberField.value = newTypedNumber; // set clicked value to input field
+    }
+
+   
 });
